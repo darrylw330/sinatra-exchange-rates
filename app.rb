3c87bd3fbbb2631fd79c4e9f0ec2257c73a1dfdd
Darryl Williams <126805312+darrylw330@users.noreply.github.com>
2023-08-04 18:55:52 +0000

    updated app.rb file added erb view files

--- app.rb
+++ app.rb
@@ -1,9 +1,39 @@
-require "sinatra"
-require "sinatra/reloader"
+# app.rb
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+require 'sinatra'
+require 'http'
+require 'json'
+
+get '/' do
+  # Fetch the list of currencies from the exchangerate.host API
+  response = HTTP.get('https://api.exchangerate.host/symbols')
+  data = JSON.parse(response.body)
+
+  # Extract the symbols from the response data
+  @symbols = data['symbols'].keys
+
+  # Render the view template (index.erb) with the list of symbols
+  erb :index
+end
+
+# New route for currency conversion options
+get '/:currency' do
+  # Fetch the list of currencies from the exchangerate.host API
+  response = HTTP.get('https://api.exchangerate.host/symbols')
+  data = JSON.parse(response.body)
+
+  # Extract the requested currency symbol from the URL parameter
+  requested_currency = params['currency'].upcase
+
+  # Check if the requested currency is available in the API's list of symbols
+  if data['symbols'].key?(requested_currency)
+    @requested_currency = requested_currency
+    @other_currencies = data['symbols'].keys.reject { |currency| currency == requested_currency }
+    
+    # Render the view template (currency_conversion.erb) with the currency conversion options
+    erb :currency_conversion
+  else
+    # If the requested currency is not available, return an error message
+    "Currency not found."
+  end
 end
